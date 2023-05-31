663a279823b9273cf7ec83855014d24e0f79d542
ad0y3z <a03zcode@ya.ru>
2023-05-31 19:46:30 +0300

    fix dumb thing about rendering sorting buttons

---
+++ src/components/Sort/const.js
@@ -0,0 +1,38 @@
+export const sortButtons = [
+	{
+		title: 'Rating',
+		subtitle: '',
+	},
+	{
+		title: 'Popularity',
+		subtitle: '/bypopularity',
+	},
+	{
+		title: 'Favorite',
+		subtitle: '/favorite',
+	},
+	{
+		title: 'Airing',
+		subtitle: '/airing',
+	},
+	{
+		title: 'Upcoming',
+		subtitle: '/upcoming',
+	},
+	{
+		title: 'TV',
+		subtitle: '/tv',
+	},
+	{
+		title: 'Movie',
+		subtitle: '/movie',
+	},
+	{
+		title: 'OVA',
+		subtitle: '/ova',
+	},
+	{
+		title: 'Special',
+		subtitle: '/special',
+	},
+]
--- src/components/Sort/index.jsx
+++ src/components/Sort/index.jsx
@@ -6,6 +6,7 @@ import {
 	pageChanged,
 	subtypeChanged,
 } from '../../pages/List/model'
+import { sortButtons } from './const'
 
 import s from './sort.module.scss'
 
@@ -20,71 +21,20 @@ export const Sort = () => {
 		getAnimeListFx()
 	}
 
-	const getByRating = () => {
-		getSorted('', 'Rating')
-	}
-	const getByPopularity = () => {
-		getSorted('/bypopularity', 'Popularity')
-	}
-
-	const getFavorite = () => {
-		getSorted('/favorite', 'Favorite')
-	}
-
-	const getAiring = () => {
-		getSorted('/airing', 'Airing')
-	}
-
-	const getUpcoming = () => {
-		getSorted('/upcoming', 'Upcoming')
-	}
-
-	const getTv = () => {
-		getSorted('/tv', 'TV')
-	}
-
-	const getMovie = () => {
-		getSorted('/movie', 'Movie')
-	}
-
-	const getOva = () => {
-		getSorted('/ova', 'OVA')
-	}
-
-	const getSpecial = () => {
-		getSorted('/special', 'Special')
-	}
-
 	return (
 		<div className={s.sort}>
 			<span>Sort by</span>
-			<button className='btn' onClick={getByRating}>
-				Rating
-			</button>
-			<button className='btn' onClick={getByPopularity}>
-				Popularity
-			</button>
-			<button className='btn' onClick={getFavorite}>
-				Favorite
-			</button>
-			<button className='btn' onClick={getAiring}>
-				Airing
-			</button>
-			<button className='btn' onClick={getUpcoming}>
-				Upcoming
-			</button>
-			<button className='btn' onClick={getTv}>
-				Tv
-			</button>
-			<button className='btn' onClick={getMovie}>
-				Movie
-			</button>
-			<button className='btn' onClick={getOva}>
-				Ova
-			</button>
-			<button className='btn' onClick={getSpecial}>
-				Special
-			</button>
+			{sortButtons.map((btn) => {
+				return (
+					<button
+						className='btn'
+						onClick={() => {
+							getSorted(btn.subtitle, btn.title)
+						}}>
+						{btn.title}
+					</button>
+				)
+			})}
 		</div>
 	)
 }
